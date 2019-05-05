1cb763672bd0dc7cda4e95c33b13f711b444f972
nikunj <nikunj.b@crestinfosystems.net>
2019-05-06 01:07:12 +0530

    Added video page Header and container design

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,19 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style lang="scss">
     /* Import Font Awesome Icons Set */
-    $fa-font-path: '~font-awesome/fonts/'
-    @import '~font-awesome/scss/font-awesome.scss'
+    $fa-font-path: '~font-awesome/fonts/';
+    @import '~font-awesome/scss/font-awesome.scss';
 
     // Import bootstrap.css
-    @import "~bootstrap/dist/css/bootstrap.min.css"
+    @import "~bootstrap/dist/css/bootstrap.min.css";
 
     // Import Main styles for this application
     @import 'assets/scss/style';
-
+    body {
+        color: #636b6f;
+        background-color: #f5f8fa;
+    }
 
 </style>
