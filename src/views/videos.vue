6a2625ba0eb355a11d02d8ff46dc02c22448d36e
nikunj <nikunj.b@crestinfosystems.net>
2019-05-06 01:30:21 +0530

    added video card design

--- src/views/videos.vue
+++ src/views/videos.vue
@@ -34,7 +34,235 @@
                         header="Category"
                         class="rounded-0"
                     >
-                        <b-card-text>Video Links</b-card-text>
+                        <b-row>
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+                        </b-row>
+                    </b-card>
+                </b-col>
+            </b-row>
+
+             <b-row>
+                <b-col md="12">
+                    <b-card
+                        header-bg-variant="transparent"
+                        header="Category"
+                        class="rounded-0"
+                    >
+                        <b-row>
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+
+                            <b-col col md="3" sm="4" xs="6" class="video-grid">
+                                <div class="video">
+                                    <div class="thumbnail">
+                                        <a href="#">
+                                            <b-img src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                        </a>
+                                    </div>
+                                    <div class="caption">
+                                        <h3>
+                                            <a href="#/video/380/high-hopes" class="">High Hopes</a>
+                                        </h3>
+                                        <p>
+                                            <a href="#/channel/239/randy-gil" class="">Randy Gil</a> <br>  7 views • 4 days ago
+                                        </p>
+                                    </div>
+                                </div>
+                            </b-col>
+                        </b-row>
                     </b-card>
                 </b-col>
             </b-row>
@@ -89,6 +317,52 @@
         padding: 10px 20px;
         color: #333333;
         border-color: #f5f8fa;
+    }
+}
+
+.video-grid {
+    .video {
+        .thumbnail {
+            background-color: #f1f1f1;
+            padding: 0;
+            margin-bottom: .5em;
+            max-height: 141px;
+            overflow: hidden;
+            display: block;
+            line-height: 1.6;
+            border: 1px solid #ddd;
+            border-radius: 0;
+            -webkit-transition: border 0.2s ease-in-out;
+            transition: border 0.2s ease-in-out;
+        }
+        .caption {
+            h3 {
+                margin-right: 20px;
+                margin-top: 0;
+                font-size: 14px;
+                margin-bottom: 1px;
+                max-width: 196px;
+                font-weight: 500;
+                a {
+                    height: 2.6em;
+                    display: block;
+                    line-height: 1.3em;
+                    overflow: hidden;
+                    position: relative;
+                    text-overflow: ellipsis;
+                    white-space: normal;
+                    word-wrap: break-word;
+                    color: #004660;
+                }
+            }
+            p {
+                font-size: .85em;
+                color: #919191;
+                a {
+                    color: #000;
+                }
+            }
+        }
     }
 }
 
